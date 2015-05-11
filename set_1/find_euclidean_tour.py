4508fe4f865c91610b124c4d531f334518c0f608
Andrea Iacono <andrea.iacono.nl@gmail.com>
2015-05-12 00:40:40 +0200

    work in progress2

--- set_1/find_euclidean_tour.py
+++ set_1/find_euclidean_tour.py
@@ -33,6 +33,12 @@ def get_matching_node_from_edge(node, edge):
     return edge[0]
 
 
+def get_edge_from_nodes(current_node, next_node, graph):
+    if (current_node, next_node) in graph:
+        return current_node, next_node
+
+    return next_node, current_node
+
 def get_next_nodes(current_node, graph):
     matching_nodes = []
     for edge in graph:
@@ -44,34 +50,32 @@ def get_next_nodes(current_node, graph):
     return matching_nodes
 
 
-def get_edge_from_nodes(current_node, next_node, graph):
-    if (current_node, next_node) in graph:
-        return current_node, next_node
-
-    return next_node, current_node
+def remove_node(eulerian_path, current_node):
+    tmp = eulerian_path[::-1]
+    tmp.remove(current_node)
+    return tmp[::-1]
 
 
 def find_next(current_node, remaining_edges, eulerian_path):
+
+    if not remaining_edges:
+        eulerian_path.append(current_node)
+        return eulerian_path
+
     print("current: ", current_node, " remaining: ", remaining_edges, " path:", eulerian_path)
-
     next_nodes = get_next_nodes(current_node, remaining_edges)
-    if not next_nodes:
-        return None
-
+    print("next_nodes of ", current_node, " are:", next_nodes, " while eulerian is ", eulerian_path)
     for next_node in next_nodes:
         eulerian_path.append(current_node)
-        remaining_edges.remove(get_edge_from_nodes(current_node, next_node, remaining_edges))
-        if not remaining_edges:
-            eulerian_path.append(next_node)
+        edge = get_edge_from_nodes(current_node, next_node, remaining_edges)
+        remaining_edges.remove(edge)
+        # result = find_next(next_node, remaining_edges, eulerian_path)
+        if find_next(next_node, remaining_edges, eulerian_path):
             return eulerian_path
-        if find_next(next_node, remaining_edges, eulerian_path) is not None:
-            return eulerian_path
-        # eulerian_path.remove(current_node)
-        # remaining_edges.append(get_edge_from_nodes(current_node, next_node, remaining_edges))
+        eulerian_path = remove_node(eulerian_path, current_node)
+        remaining_edges.append(edge)
 
-    print("finished loop: current: ", current_node, " remaining: ", remaining_edges, " path:", eulerian_path)
-
-
+    print("finished loop; sending back None. Path ", eulerian_path)
     return None
 
 
@@ -79,7 +83,7 @@ def find_eulerian_tour(graph):
     edges_num = count(graph)
 
     for node in edges_num.keys():
-        print("Starting node: ", node)
+        print("first node: ", node)
         tour = list(graph)
         eulerian_path = []
         if find_next(node, tour, eulerian_path):
@@ -94,7 +98,7 @@ test_set2 = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9), (2,
 test_set3 = [(1, 13), (1, 6), (6, 11), (3, 13), (8, 13), (0, 6), (8, 9), (5, 9), (2, 6), (6, 10), (7, 9), (1, 12), (4, 12) ,(5, 14), (0, 1), (2, 3), (4, 11), (6, 9), (7, 14), (10, 13)]
 test_set4 = [(8, 16), (8, 18), (16, 17), (18, 19), (3, 17), (13, 17), (5, 13), (3, 4), (0, 18), (3, 14), (11, 14), (1, 8), (1, 9), (4, 12), (2, 19), (1, 10), (7, 9), (13, 15), (6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
 
-print(count(test_set3))
+# print(count(test_set4))
 print("solution= ", find_eulerian_tour(test_set3))
 # print(find_eulerian_tour(test_set2))
 # print(find_eulerian_tour(test_set3))
